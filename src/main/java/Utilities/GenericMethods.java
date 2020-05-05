722314984457865d7f5a323ae00fc657ffa3ae40
kannanv82 <kannan0782@gmail.com>
2020-05-05 14:02:02 +0530

    Un used code has been removed

--- src/main/java/Utilities/GenericMethods.java
+++ src/main/java/Utilities/GenericMethods.java
@@ -18,22 +18,7 @@ public class GenericMethods {
 		wait = new WebDriverWait(driver, 50);
 	}
 	public boolean scrollToText(String Text, WebElement ele) {
-//		String scrollViewContainer_finder = "new UiSelector().className('android.widget.ListView')";
-//		String neededElement_finder ="new UiSelector().textContains(\"REORDER\")"; 
-//		
-//		try {
-//		ele = driver.findElement(By.xpath("//android.widget.FrameLayout[contains(@resource-id,'content')]/android.webkit.WebView/android.webkit.WebView"));
-//		}
-//		catch(Exception e) {
-//			
-//		}
-//		AndroidElement ele1 = driver.findElement(By.xpath("//android.webkit.WebView"));
 		try {
-
-//			((AndroidElement) ele)
-//					.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView("
-//							+ "new UiSelector().textContains(\""+str+"\"));");
-
 			wait.until(ExpectedConditions.visibilityOf(ele));
 			((AndroidElement) ele).findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).setMaxSearchSwipes("+40+").scrollIntoView("
 					+ "new UiSelector().textContains(\""+Text+"\"));");
